26692300310c7ba89a751fc97fc2a4e863e9e60c
LC1007 <tonyg7212@gmail.com>
2023-05-17 14:59:49 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,19 @@
+let personOne = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: "John Doe"
+}
+
+let personTwo = {
+    firstName: "Mark",
+    lastName: "Doe",
+    fullName: "Mark Doe"
+}
+
+
+console.log(personOne);
+console.log(personTwo);
+
+let newArr = [personOne]
+
+console.log(newArr);
